cc8720aef419979dcf0214de7694ae4af2b02037
Zoha <zoha.banam@gmail.com>
2026-02-07 21:12:28 +0330

    optimize performance with self-hosted fonts and compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { compression } from "vite-plugin-compression2";
 export default defineConfig({
   base: "/",
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    compression({ algorithms: ["gzip", "brotliCompress"] }),
+  ],
+  build: {
+    cssMinify: true,
+    minify: true,
+  },
 });
